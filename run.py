5312d187e38408dfc877516379e8d3a6c1c4dd85
RadheyaM <radheyamansel@hotmail.com>
2022-09-22 14:08:52 +0000

    Implement extend code to have a base page displaying appropriate content based on URL

--- run.py
+++ run.py
@@ -6,7 +6,26 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """renders the home page template"""
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    """renders the about page template"""
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    """renders the contact page template"""
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    """renders the contact page template"""
+    return render_template("careers.html")
 
 
 if __name__ == "__main__":
